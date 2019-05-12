0e54706e91373f132f3c503d8568ae355baef1a7
dais <daispaysagist@gmail.com>
2019-05-12 11:16:45 +0300

    added .vimrc and fixed bugs

--- install.sh
+++ install.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
-# NOTE: This repo's folder should be placed in the user's home directory (i.e. ~ or /home/myusername) .
+# NOTE: This repo's folder should be placed in the user's home directory (i.e. ~ or /home/myusername).
 
-printf "\nUsage:\n    1.  This repo's folder should be placed at your home directory:\n        $HOME\n        Is this the case?\n    2.  Run this script from the repo's directory.\n\n"
+printf "\nUsage:\n    1.  Place repo directory in your Linux user home directory: $HOME or simply ~.\n    2.  cd into the repo.\n    3.  run the install.sh script.\n\n"
 
-read -p "WARNING: This will overwrite your local dotfiles. Are you sure? [y/n]  " answer
+# Get user approval
+read -p "WARNING: This will overwrite your local dotfiles. Are you sure? ['y' to accept, any key to abort]  " answer
 
-if [ $answer = 'n' ]; then
+if [[ $answer != 'y' ]]; then
 	printf "\nInstallation Aborted.\n\n"
 	exit	
-elif [ $answer = 'y' ]; then
-	printf "\nSuccessfully installed dotfiles in:\n"
-	if cp -r ./user_home_directory/ $HOME ; then
-		echo "$HOME"
-	else
-		printf "\nSomething went wrong. Troubleshooting:\n	1.  Is this repo's directory located at your home folder: $HOME ?\n	2.  Are you running this script from the repo's directory?\n\n"		
+else	
+	printf "\nInstalling the following dotfiles:\n"
+	if ! cp -rvT ./user_home_directory $HOME; then
+	#else
+		printf "\nSomething went wrong. Troubleshooting:\n	1.  Is this repo's directory located at your home folder: $HOME ?\n	2.  Are you running this script from the repo's directory?\n\n"
 	fi
-else
-	echo "\nI did not understand that. Please rerun this installation script and type 'y'.\n\n"
 fi
 
-printf "\nDone.\n\n"
+printf "\nInstallation complete.\n\n"
